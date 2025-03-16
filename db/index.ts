9224dd87af0ec2129f6a84169071b1145c2cfbed
hemanth <applicationcomputer257@gmail.com>
2025-03-17 01:23:02 +0530

    removed unwanted components and pages

--- db/index.ts
+++ db/index.ts
@@ -1,20 +1,3 @@
-// import mongoose from "mongoose";
-
-// let isConnected = false;
-
-// export const connectToDatabase = async () => {
-//   if (isConnected) return;
-
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI as string);
-//     isConnected = true;
-//     console.log("Connected to MongoDB");
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//     throw new Error("Failed to connect to database");
-//   }
-// };
-
 import mongoose from "mongoose";
 
 interface GlobalMongoose {
